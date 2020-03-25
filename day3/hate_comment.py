448754e2f6f9c946b0734f04c03f38abdc28f825
RoshanSalian <roshansalian1997@gmail.com>
2020-03-25 21:56:14 +0530

    day3 small

---
+++ day3/hate_comment.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 14:28:05 2020
+
+@author: roshan
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+
+# https://medium.com/@nupurbaghel/toxic-comment-classification-f6e075c3487a
